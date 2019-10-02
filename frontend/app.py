c029517c69c9422315fa4d2b609e45dd0bf66f3e
vyrzdev <55830610+vyrzdev@users.noreply.github.com>
2019-10-02 13:08:34 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -7,17 +7,19 @@ redis_server_address = "35.210.227.246"
 redis_server_port = "80"
 conn = redis.Redis(host=redis_server_address, port=redis_server_port)
 app = dash.Dash(__name__)
-
+#Below Function requires renovation. 
 def refresh_table():
     dataframe = conn.hgetall("12VCo1")
     table =[html.Tr(className="table-header", children=[html.Th("Student"), html.Th("Total Contributed")] )] + [html.Tr(className="table-row", children = [html.Td(str(x[0])), html.Td(str(x[1]))]) for x in dataframe]
     return table
-
+#app.layout, is an attribute of the webpage which stores different html objects as a list, data inside the children variables of each
+#html object can be modified using callbacks which take input from interactive components and output to the children vars.
 app.layout = html.Div(id="parent", className="parent", children=[
         html.Div(className = "main", id="main"),
         dcc.Interval(id="interval-component", interval=(1*1000), n_intervals=0)
     ])
-
+#Takes input from id='interval-component', from the variable n_intervals, and outputs to the children var of id='main'
+#The value outputted is the array returned by the defined function.
 @app.callback(ddp.Output('main', 'children'),
               [ddp.Input('interval-component', 'n_intervals')])
 def refresh_main(n):
