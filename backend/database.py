ab182c80de473d4b73d868ee30fa10022b57e3ff
manfromth3m0n <maxstantay@gmail.com>
2019-10-10 12:49:23 +0100

    refactored database.py

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,27 @@
 import redis
-#import flask
+import random
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+students = []
+
 def create12VCo1db():
+	#Opens a list of names and makes a list out of them
 	f = open('students.txt', 'r')
-	students = []
 	for line in f:
 	    students.append(line.strip('\n'))
-
-	#print(students[0])
 	
+	#sets the base values for everyone
 	data = {"weekContribution":"0.00", "totalContribution":"0.00"}
 
+	#writes names and data to the database
 	for i in range(0, len(students)):
 		r.hmset(students[i], data)
 
-	#r.hset('Max Stanford-Taylor', 'weekContribution', '0.00')
-
 create12VCo1db()
 
-print(r.hgetall('Max Stanford-Taylor'))
+#Prints a value from the newly entered data
+student = students[random.randint(0, len(students) -1)]
+print(student + ' : ' + str(r.hgetall(student)))
 
 
