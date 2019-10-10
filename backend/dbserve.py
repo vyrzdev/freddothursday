afa71d1c81ab856e5d878c26db8579c517ee82ba
manfromth3m0n <maxstantay@gmail.com>
2019-10-10 14:20:23 +0100

    server stuff

--- backend/dbserve.py
+++ backend/dbserve.py
@@ -1,5 +1,5 @@
 import redis
-from flask import Flask
+from flask import Flask, requests
 
 r = redis.Redis('localhost')
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 
 #Use 'keys *' to get all keys from the db
 #The gather all keys with the specific class value
+
+def byteCodeDictToStr(diction):
+	newdict = {}
+	for key in diction.keys():
+		newdict[key.decode("utf-8")] = diction[key].decode("utf-8")
+	return newdict
 
 @app.route('/getClassMembers/<classname>')
 def getClassMembers(classname):
@@ -16,13 +22,24 @@ def getClassMembers(classname):
 		hash = r.hgetall(key)
 		if hash[b'class'] == classname:
 			members.append(key)
-
 	return members
 
 @app.route('/getStudentData/<studentname>')
 def getStudentData(studentname):
 	data = r.hgetall(studentname)
 	return data
+
+@app.route('/updateWeekTotal/<studentname>')
+def updateStudentData(studentusername):
+	if request.args.get('contrib'):
+		weeklyContribToAdd = request.args.get('contrib')
+		student = r.hgetall(studentname)
+		student['weeklyContribution'] = student['weeklyContribution'] + weeklyContribToAdd
+		student['totalContribution'] = student['totalContribution'] + weeklyContribToAdd
+		r.hmset(studentname, student)
+
+#uuid update
+#register user
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ backend/test.py
@@ -0,0 +1,7 @@
+hash = {b'weekContribution': b'0.00', b'totalContribution': b'0.00', b'class': b'12VCo1'}
+for i in hash:
+	dict = {}
+	m =  i.decode('UTF-8')
+	dict.append(m)
+
+print(dict)
